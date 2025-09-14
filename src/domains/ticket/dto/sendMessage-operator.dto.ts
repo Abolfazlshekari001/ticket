faef56d5378feeaf5979098ec26b4e42a102a72a
abolfazl <abolfazlsheka1@gmail.com>
2025-09-14 13:13:54 -0700

    improve

--- src/domains/ticket/dto/sendMessage-operator.dto.ts
+++ src/domains/ticket/dto/sendMessage-operator.dto.ts
@@ -11,7 +11,7 @@ export class sendMessageOperatorSubmitDto {
     @IsNotEmpty()
     operatorId: string;
 
-    @ApiProperty()
+   // @ApiProperty()
     @IsNotEmpty()
     @IsUUID()
     ticketId: string;
